2ac817b386fed1adc45ab9afdf3549a8b4c99399
Chris Hannam <ch@chrishannam.co.uk>
2023-02-26 20:40:07 +0000

    Add logger

---
+++ race_strategist/otel_helpers.py
@@ -0,0 +1,22 @@
+import functools
+import os
+
+from opentelemetry import trace
+
+
+def time_method(func):
+    """
+    Decorator to send span timing to Jaeger agent.
+    """
+
+    @functools.wraps(func)
+    def opentelemetry_timer_wrapper(*args, **kwargs):
+        # bypass timing process
+        if not os.getenv('OPENTELEMETRY_ENDPOINT'):
+            return func(*args, **kwargs)
+
+        tracer = trace.get_tracer(__name__)
+        with tracer.start_as_current_span(func.__name__):
+            return func(*args, **kwargs)
+
+    return opentelemetry_timer_wrapper
